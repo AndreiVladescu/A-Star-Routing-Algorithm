21f992398db7e3609983fb4022605fe8012df0cb
AndreiVladescu <rudolfal2lea@yahoo.com>
2022-12-16 10:15:52 +0200

    Updated for viewing

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
 from network_devices import *
 import matplotlib.pyplot as plt
 import networkx as nx
+import PIL
 
-GRAPH = {'A': [['B', 1], ['C', 1], ['D', 2]],
-         'B': [['C', 2], ['E', 1]],
-         'C': [['F', 1]],
-         'D': [['C', 2], ['F', 1]],
-         'E': [['F', 1]],
-         'F': [['G', 1]],
-         'G': []
-         }
+# Image URLs for graph nodes
+icons = {
+    "router": "imgs/router.jpg",
+    "pc": "imgs/pc.jpg",
+}
+
+# Load images
+images = {k: PIL.Image.open(fname) for k, fname in icons.items()}
+
+R_A = NetworkDevice(type='router', name='R_A')
+R_B = NetworkDevice(type='router', name='R_B')
+R_C = NetworkDevice(type='router', name='R_C')
+PC_A = NetworkDevice(type='pc', name='PC_A')
+PC_B = NetworkDevice(type='pc', name='PC_B')
+
+COM_1 = NetworkEdge(type='Fiber')
+COM_2 = NetworkEdge(type='Fiber')
+COM_3 = NetworkEdge(type='Fiber')
+COM_4 = NetworkEdge(type='Serial')
+COM_5 = NetworkEdge(type='Serial')
+
+R_C.bandwidth = 1
+
+NETWORK_LIST = {R_A, R_B, R_C, PC_A, PC_B}
+
+NETWORK_GRAPH = {R_A: [[R_B, COM_1], [R_C, COM_4]],
+                 R_B: [[PC_B, COM_3]],
+                 R_C: [[PC_B, COM_5]],
+                 PC_A: [[R_A, COM_2]],
+                 PC_B: []
+                 }
 
 INF = 10000
 VISITED = []
 
-G = {'A': 0, 'B': INF, 'C': INF, 'D': INF, 'E': INF, 'F': INF, 'G': INF}
-parent = {'A': '', 'B': '', 'C': '', 'D': '', 'E': '', 'F': '', 'G': ''}
-bandwidth_h = {'A': 1, 'B': 1, 'C': 2, 'D': 2, 'E': 2, 'F': 3, 'G': 0}
-load_h = {'A': 0, 'B': 0, 'C': 1, 'D': 4, 'E': 3, 'F': 0, 'G': 2}
-delay_h = {'A': 2, 'B': 5, 'C': 2, 'D': 5, 'E': 2, 'F': 1, 'G': 0}
+G = {PC_A: 0, R_A: INF, R_B: INF, R_C: INF, PC_B: INF}
+parent = {R_A: None, R_B: None, R_C: None, PC_A: None, PC_B: None}
+h_net = {R_A: R_A.get_weight(), R_B: R_B.get_weight(), R_C: R_C.get_weight(), PC_A: PC_A.get_weight(),
+         PC_B: PC_B.get_weight()}
+hops = 0
 
 
-def update_g(node, next_node, dist):
-    if G[node] + dist < G[next_node]:
-        G[next_node] = G[node] + dist
+def update_g(node, next_node, path):
+    if G[node] + path.weight < G[next_node]:
+        G[next_node] = G[node] + path.weight
         parent[next_node] = node
 
 
@@ -32,50 +56,106 @@ def g(n):
 
 
 def h(n):
-    return bandwidth_h[n] + delay_h[n] + load_h[n]
+    return h_net[n]
 
 
 def f(n):
     return h(n) + g(n)
 
 
-def expand(n):
-    for next in GRAPH[n]:
+def expand(n, target):
+    for next in NETWORK_GRAPH[n]:
         update_g(n, next[0], next[1])
-    return [next[0] for next in GRAPH[n]]
+    # return [next[0] for next in NETWORK_GRAPH[n]]
+    reachable_device_list = []
+    for next in NETWORK_GRAPH[n]:
+        if next[0].is_routable or next[0] == target:
+            reachable_device_list.append(next[0])
+    return reachable_device_list
 
 
-iter = 0
+def a_star(node, target):
+    global hops
+    hops += 1
+    # 32 Hops Max
+    if hops > 32:
+        return False
 
-
-def a_star(node, scop):
-    global iter
-    iter += 1
     global VISITED
     VISITED.append(node)
-    if node == scop:
+    if node == target:
         return True
 
-    M = expand(node)
-    M.sort(key=f)  # incercati key=h si key=g. Explicati diferentele.
+    M = expand(node, target)
+    M.sort(key=f)
 
     for m in M:
         if m not in VISITED:
-            if a_star(m, scop):
+            if a_star(m, target):
                 return True
     return False
 
 
-start = 'A'
-goal = 'G'
-a_star(start, goal)
+def main():
+    global parent
+    start = PC_A
+    goal = PC_B
+    a_star(start, goal)
 
-path = []
-child = goal
-while child != start:
-    path.append(child)
-    child = parent[child]
+    path = []
+    child = goal
+    while child != start:
+        path.append(child.name)
+        child = parent[child]
 
-path.append(start)
-path.reverse()
-print("Calea este {} si a fost gasita in {} iteratii.".format(path, iter))
+    path.append(start.name)
+    path.reverse()
+    print("Calea este {} si a fost gasita in {} iteratii.".format(path, hops))
+
+    Graph = nx.Graph()
+    Graph.add_node(R_A.name, image=images["router"])
+    Graph.add_node(R_B.name, image=images["router"])
+    Graph.add_node(R_C.name, image=images["router"])
+    Graph.add_node(PC_A.name, image=images["pc"])
+    Graph.add_node(PC_B.name, image=images["pc"])
+
+    Graph.add_edge(R_A.name, R_B.name, weight=COM_1.weight)
+    Graph.add_edge(R_A.name, R_C.name, weight=COM_4.weight)
+    Graph.add_edge(R_B.name, PC_B.name, weight=COM_3.weight)
+    Graph.add_edge(R_C.name, PC_B.name, weight=COM_5.weight)
+    Graph.add_edge(PC_A.name, R_A.name, weight=COM_2.weight)
+
+    pos = nx.spring_layout(Graph, seed=1734289230)
+    fig, ax = plt.subplots()
+    options = {
+        "node_size": 3000,
+        "node_color": "white",
+        #"edgecolors": "black",
+        "linewidths": 5,
+        "width": 5,
+    }
+    nx.draw_networkx_edges(
+        Graph,
+        pos=pos,
+        ax=ax,
+        min_source_margin=15,
+        min_target_margin=15,
+    )
+    nx.draw_networkx(Graph, pos, **options)
+    tr_figure = ax.transData.transform
+    tr_axes = fig.transFigure.inverted().transform
+
+    icon_size = (ax.get_xlim()[1] - ax.get_xlim()[0]) * 0.08
+    icon_center = icon_size / 2.0
+
+    for n in Graph.nodes:
+        xf, yf = tr_figure(pos[n])
+        xa, ya = tr_axes((xf, yf))
+        a = plt.axes([xa - icon_center, ya - icon_center, icon_size, icon_size])
+        a.imshow(Graph.nodes[n]["image"])
+        a.axis("off")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
--- network_devices.py
+++ network_devices.py
@@ -1,66 +1,68 @@
-class NetworkDevice:
-    counter_router = 1
-    counter_pc = 1
+counter_pc = 1
+counter_router = 1
 
+class NetworkDevice:
     is_routable = False
 
     bandwidth = 1
     latency = 1
     load = 1
 
-    def __init__(self, type, name =''):
+    def __init__(self, type, name=''):
+        global counter_router
+        global counter_pc
         if type == 'router':
             self.type = 'router'
             self.image = 'imgs/router.jpg'
             self.is_routable = True
             if name == '':
-                self.name = 'R' + str(self.counter_router)
-                self.counter_router += 1
+                self.name = 'R' + str(counter_router)
+                counter_router += 1
             else:
                 self.name = name
         else:
             self.type = 'pc'
             self.image = 'imgs/pc.jpg'
             if name == '':
-                self.name = 'PC' + str(self.counter_pc)
-                self.counter_pc += 1
+                self.name = 'PC' + str(counter_pc)
+                counter_pc += 1
             else:
                 self.name = name
-
-    def can_route(self):
-        return self.is_routable
 
     def get_weight(self):
         return self.bandwidth + self.latency + self.load
 
-    def get_name(self):
+    def __int__(self):
+        return self.get_weight()
+
+    def __str__(self):
         return self.name
 
-    def get_type(self):
-        return self.type
+    def _cmp(self, other):
+        return int(self) - int(other)
+
 
 class Firewall:
     def __int__(self, rules):
-        self.firewall_rules =  rules
+        self.firewall_rules = rules
         if len(rules) > 0:
             self.is_active = True
         else:
             self.is_active = False
-    def get_rules(self):
-        return self.firewall_rules
+
     def can_block(self, protocol):
         return self.firewall_rules[protocol.get_name()]
+
 
 class Protocol:
     def __init__(self, name):
         self.name = name
 
-    def get_name(self):
-        return self.name
 
 class NetworkEdge:
     weight = 0
-    def __init__(self, type, override = 0):
+
+    def __init__(self, type, override=0):
         self.type = type
 
         if override == 0:
@@ -78,16 +80,21 @@ class NetworkEdge:
                 self.weight = 40
         else:
             self.weight = override
-    def get_weight(self):
-        return self.weight
-    def get_type(self):
-        return self.type
+
 
 class Message:
     def __init__(self, protocol, data):
         self.protocol = protocol
         self.data = data
-    def get_protocol(self):
-        return self.protocol
-    def get_data(self):
-        return self.data
+
+
+class Network:
+
+    def __init__(self):
+        pass
+
+    def add_node(self, network_node):
+        self.node_list.append(network_node)
+
+    def add_path(self, node1, node2, path):
+        pass
