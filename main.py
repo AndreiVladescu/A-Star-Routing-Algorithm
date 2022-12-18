5bacbe62585a799e030d6f09a0848df4c241bb8b
AndreiVladescu <rudolfal2lea@yahoo.com>
2022-12-18 10:10:07 +0200

    Encapsulated data inside classes + Firewall

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
 from network_devices import *
-import matplotlib.pyplot as plt
-import networkx as nx
-import PIL
-
-# Image URLs for graph nodes
-icons = {
-    "router": "imgs/router.jpg",
-    "pc": "imgs/pc.jpg",
-}
-
-# Load images
-images = {k: PIL.Image.open(fname) for k, fname in icons.items()}
 
 R_A = NetworkDevice(type='router', name='R_A')
 R_B = NetworkDevice(type='router', name='R_B')
@@ -30,145 +18,54 @@ COM_6 = NetworkEdge(type='Ethernet')
 COM_7 = NetworkEdge(type='Ethernet')
 COM_8 = NetworkEdge(type='Ethernet')
 
-NETWORK_GRAPH = {R_A: [[R_B, COM_1], [R_C, COM_4]],
-                 R_B: [[R_D, COM_3]],
-                 R_C: [[R_D, COM_5]],
-                 R_D: [[PC_B, COM_6], [PC_C, COM_7], [PC_D, COM_8]],
-                 PC_A: [[R_A, COM_2]],
-                 PC_B: [],
-                 PC_C: [],
-                 PC_D: []
-                 }
-
-INF = 10000
-VISITED = []
-
-G = {PC_A: 0, R_A: INF, R_B: INF, R_C: INF, R_D: INF, PC_B: INF, PC_C: INF, PC_D: INF}
-parent = {R_A: None, R_B: None, R_C: None, R_D: None, PC_A: None, PC_B: None, PC_C: None, PC_D: None}
-h_net = {R_A: R_A.get_weight(), R_B: R_B.get_weight(), R_C: R_C.get_weight(), R_D: R_D.get_weight(),
-         PC_A: PC_A.get_weight(),
-         PC_B: PC_B.get_weight(),
-         PC_C: PC_C.get_weight(),
-         PC_D: PC_D.get_weight()}
-hops = 0
-
-
-def update_g(node, next_node, path):
-    if G[node] + path.weight < G[next_node]:
-        G[next_node] = G[node] + path.weight
-        parent[next_node] = node
-
-
-def g(n):
-    return G[n]
-
-
-def h(n):
-    return h_net[n]
-
-
-def f(n):
-    return h(n) + g(n)
-
-
-def expand(n, target):
-    for next in NETWORK_GRAPH[n]:
-        update_g(n, next[0], next[1])
-    # return [next[0] for next in NETWORK_GRAPH[n]]
-    reachable_device_list = []
-    for next in NETWORK_GRAPH[n]:
-        if next[0].is_routable or next[0] == target:
-            reachable_device_list.append(next[0])
-    return reachable_device_list
-
-
-def a_star(node, target):
-    global hops
-    hops += 1
-    # 32 Hops Max
-    if hops > 32:
-        return False
-
-    global VISITED
-    VISITED.append(node)
-    if node == target:
-        return True
-
-    M = expand(node, target)
-    M.sort(key=f)
-
-    for m in M:
-        if m not in VISITED:
-            if a_star(m, target):
-                return True
-    return False
+# Generic Communication Cables
+GEN_COM_Fiber = NetworkEdge(type='Fiber')
+GEN_COM_Serial = NetworkEdge(type='Serial')
+GEN_COM_Ethernet = NetworkEdge(type='Ethernet')
+GEN_COM_Satellite = NetworkEdge(type='Satellite')
+GEN_COM_WiFi = NetworkEdge(type='WiFi')
+GEN_COM_GSM = NetworkEdge(type='GSM')
 
 
 def main():
-    global parent
-    start = PC_A
-    goal = PC_C
-    a_star(start, goal)
 
-    path = []
-    child = goal
-    while child != start:
-        path.append(child.name)
-        child = parent[child]
+    protocol = Protocol('FTP')
+    message = Message(protocol=protocol, data='USER')
+    # If detect data == USER from FTP protocol, then drop it
+    rule = [protocol, 'USER', True]
+    r_c_firewall = Firewall()
+    r_c_firewall.add_rule(rule)
+    r_c_firewall.is_active = True
 
-    path.append(start.name)
-    path.reverse()
-    print("Calea este {} si a fost gasita in {} iteratii.".format(path, hops))
+    R_C.firewall = r_c_firewall
 
-    Graph = nx.Graph()
-    Graph.add_node(R_A.name, image=images["router"])
-    Graph.add_node(R_B.name, image=images["router"])
-    Graph.add_node(R_C.name, image=images["router"])
-    Graph.add_node(R_D.name, image=images["router"])
-    Graph.add_node(PC_A.name, image=images["pc"])
-    Graph.add_node(PC_B.name, image=images["pc"])
-    Graph.add_node(PC_C.name, image=images["pc"])
-    Graph.add_node(PC_D.name, image=images["pc"])
+    network = Network(start=PC_A, goal=PC_B, message=message)
 
-    Graph.add_edge(R_A.name, R_B.name, weight=COM_1.weight)
-    Graph.add_edge(R_A.name, R_C.name, weight=COM_4.weight)
-    Graph.add_edge(R_B.name, R_D.name, weight=COM_3.weight)
-    Graph.add_edge(R_C.name, R_D.name, weight=COM_5.weight)
-    Graph.add_edge(R_D.name, PC_B.name, weight=COM_6.weight)
-    Graph.add_edge(PC_A.name, R_A.name, weight=COM_2.weight)
-    Graph.add_edge(R_D.name, PC_C.name, weight=COM_7.weight)
-    Graph.add_edge(R_D.name, PC_D.name, weight=COM_8.weight)
+    network.add_nodes([PC_A, PC_B, PC_C, PC_D, R_A, R_B, R_C, R_D])
+    '''NETWORK_GRAPH = {R_A: [[R_B, COM_1], [R_C, COM_4]],
+                             R_B: [[R_D, COM_3], [R_A, COM_1]],
+                             R_C: [[R_D, COM_5], [R_A, COM_4]],
+                             R_D: [[PC_B, COM_6], [PC_C, COM_7], [PC_D, COM_8]],
+                             PC_A: [[R_A, COM_2]],
+                             PC_B: [],
+                             PC_C: [],
+                             PC_D: []
+                             }'''
 
-    pos = nx.spring_layout(Graph, seed=1734289230)
-    fig, ax = plt.subplots()
-    options = {
-        "node_size": 3000,
-        "node_color": "white",
-        "edgecolors": "black",
-        "linewidths": 5,
-        "width": 5,
-    }
-    nx.draw_networkx_edges(
-        Graph,
-        pos=pos,
-        ax=ax,
-        min_source_margin=15,
-        min_target_margin=15,
-    )
-    nx.draw_networkx(Graph, pos, **options)
-    tr_figure = ax.transData.transform
-    tr_axes = fig.transFigure.inverted().transform
+    network.add_paths([[PC_A, R_A, GEN_COM_Fiber],
+                       [R_A, R_B, GEN_COM_Fiber],
+                       [R_A, R_C, GEN_COM_Serial],
+                       [R_B, R_D, GEN_COM_Fiber],
+                       [R_C, R_D, GEN_COM_Serial],
+                       [R_D, PC_B, GEN_COM_Ethernet],
+                       [R_D, PC_C, GEN_COM_Ethernet],
+                       [R_D, PC_D, GEN_COM_Ethernet],
+                       #[R_A, R_D, GEN_COM_Satellite]
+                       ])
 
-    icon_size = (ax.get_xlim()[1] - ax.get_xlim()[0]) * 0.05
-    icon_center = icon_size / 2.0
-
-    for n in Graph.nodes:
-        xf, yf = tr_figure(pos[n])
-        xa, ya = tr_axes((xf, yf))
-        a = plt.axes([xa + icon_center / 1.2, ya - icon_center, icon_size, icon_size])
-        a.imshow(Graph.nodes[n]["image"])
-        a.axis("off")
-    plt.show()
+    network.setup()
+    # network.NETWORK_GRAPH = NETWORK_GRAPH
+    network.run()
 
 
 if __name__ == "__main__":
--- network_devices.py
+++ network_devices.py
@@ -1,12 +1,45 @@
+import networkx as nx
+import PIL
+import matplotlib.pyplot as plt
+
 counter_pc = 1
 counter_router = 1
 
+
+class Protocol:
+    def __init__(self, name):
+        self.name = name
+
+
+class Message:
+    def __init__(self, protocol, data):
+        self.protocol = protocol
+        self.data = data
+
+
+class Firewall:
+    firewall_rules = {}
+
+    def __int__(self, rules):
+        self.firewall_rules = rules
+        if len(rules) > 0:
+            self.is_active = True
+        else:
+            self.is_active = False
+
+    def add_rule(self, rule):
+        self.firewall_rules[rule[0]] = [rule[1], rule[2]]
+
+    def can_block(self, protocol):
+        return self.firewall_rules[protocol.get_name()]
+
+
 class NetworkDevice:
     is_routable = False
-
     bandwidth = 1
     latency = 1
     load = 1
+    firewall = None
 
     def __init__(self, type, name=''):
         global counter_router
@@ -41,22 +74,20 @@ class NetworkDevice:
     def _cmp(self, other):
         return int(self) - int(other)
 
-
-class Firewall:
-    def __int__(self, rules):
-        self.firewall_rules = rules
-        if len(rules) > 0:
-            self.is_active = True
-        else:
-            self.is_active = False
-
-    def can_block(self, protocol):
-        return self.firewall_rules[protocol.get_name()]
-
-
-class Protocol:
-    def __init__(self, name):
-        self.name = name
+    def is_blocked(self, message):
+        if self.firewall is None:
+            print('{} let {} message \'{}\' pass through'.format(self.name, message.protocol.name, message.data))
+            return False
+        '''
+        for rule in self.firewall.firewall_rules:
+            if rule[0] == message.protocol and rule[1] == message.data and rule[2] == True:
+                return True'''
+        for key in self.firewall.firewall_rules:
+            if message.protocol == key and message.data == self.firewall.firewall_rules[key][0] and self.firewall.firewall_rules[key][1] == True:
+                print('{} blocked {} message \'{}\''.format(self.name, message.protocol.name, message.data))
+                return True
+        print('{} let {} message \'{}\' pass through'.format(self.name, message.protocol.name, message.data))
+        return False
 
 
 class NetworkEdge:
@@ -82,19 +113,168 @@ class NetworkEdge:
             self.weight = override
 
 
-class Message:
-    def __init__(self, protocol, data):
-        self.protocol = protocol
-        self.data = data
-
-
 class Network:
+    message = None
+    paths = []
+    node_list = []
+    # Image URLs for graph nodes
+    icons = {
+        "router": "imgs/router.jpg",
+        "pc": "imgs/pc.jpg",
+    }
 
-    def __init__(self):
-        pass
+    NETWORK_GRAPH = dict()
+
+    INF = 10000
+    VISITED = []
+
+    hop_count = 0
+
+    def __init__(self, start, goal, message):
+        self.big_G = {}
+        self.parent = {}
+        self.Graph = nx.Graph()
+        self.images = {k: PIL.Image.open(fname) for k, fname in self.icons.items()}
+        self.start = start
+        self.goal = goal
+        self.message = message
 
     def add_node(self, network_node):
         self.node_list.append(network_node)
 
+    def add_nodes(self, network_nodes):
+        for node in network_nodes:
+            self.add_node(network_node=node)
+
     def add_path(self, node1, node2, path):
-        pass
+        self.paths.append([node1, node2, path])
+
+    def add_paths(self, paths_list):
+        for path in paths_list:
+            self.paths.append(path)
+            self.paths.append([path[1], path[0], path[2]])
+
+    def setup(self):
+        for node in self.node_list:
+            self.big_G[node] = 0 if node == self.start else self.INF
+            self.parent[node] = None
+
+        for node in self.node_list:
+            self.Graph.add_node(node, image=self.images["router"] if node.type == 'router' else self.images['pc'])
+        # (PC_A.name, R_A.name, weight=COM_2.weight)
+        for path in self.paths:
+            self.Graph.add_edge(path[0], path[1], weight=path[2].weight)
+            self.Graph.add_edge(path[1], path[0], weight=path[2].weight)
+            self.NETWORK_GRAPH[path[0]] = []
+            self.NETWORK_GRAPH[path[1]] = []
+
+        for path in self.paths:
+            self.NETWORK_GRAPH[path[0]].append([path[1], path[2]])
+            self.NETWORK_GRAPH[path[1]].append([path[0], path[2]])
+
+    def update_g(self, node, next_node, path):
+        if self.big_G[node] + path.weight < self.big_G[next_node]:
+            self.big_G[next_node] = self.big_G[node] + path.weight
+            self.parent[next_node] = node
+
+    def g(self, n):
+        return self.big_G[n]
+
+    def h(self, n):
+        return n.get_weight()
+
+    def f(self, n):
+        return self.h(n) + self.g(n)
+
+    def expand(self, n, target):
+        for next in self.NETWORK_GRAPH[n]:
+            self.update_g(n, next[0], next[1])
+        # return [next[0] for next in NETWORK_GRAPH[n]]
+        reachable_device_list = []
+        for next in self.NETWORK_GRAPH[n]:
+            if (next[0].is_routable or next[0] == target) and not next[0].is_blocked(self.message):
+                reachable_device_list.append(next[0])
+        return reachable_device_list
+
+    def a_star(self, node, target):
+        self.hop_count += 1
+        # 32 Hops Max
+        if self.hop_count > 32:
+            return False
+
+        self.VISITED.append(node)
+        if node == target:
+            return True
+
+        M = self.expand(node, target)
+        M.sort(key=self.f)
+
+        for m in M:
+            if m not in self.VISITED:
+                if self.a_star(m, target):
+                    return True
+        return False
+
+    def getKeyPaths(self, obj):
+        return obj[0].name
+
+    def run(self):
+        self.a_star(self.start, self.goal)
+
+        path = []
+        child = self.goal
+        while child != self.start:
+            path.append(child.name)
+            child = self.parent[child]
+
+        path.append(self.start.name)
+        path.reverse()
+        print("Message took the route {} in {} hops.".format(path, self.hop_count))
+        print(
+            "Station {} received {} message '{}' from station {}".format(self.goal.name, self.message.protocol.name,
+                                                                         self.message.data, self.start.name))
+        colors = []
+        for i in range(len(self.paths)):
+            colors.append("black")
+        ## Experimental
+        '''
+        self.paths.sort(key=self.getKeyPaths)
+        for i in range(len(path) - 1):
+            for j, path_i in enumerate(self.paths):
+                if path_i[0].name == path[i] and path_i[1].name == path[i + 1]:
+                    colors[j] = "red"
+        '''
+        # colors[1] = "red"
+        pos = nx.spring_layout(self.Graph, seed=1734289230)
+        fig, ax = plt.subplots()
+        options = {
+            "node_size": 3000,
+            "node_color": "white",
+            "edgecolors": "black",
+            "edge_color": colors,
+            "linewidths": 5,
+            "width": 5,
+        }
+        nx.draw_networkx_edges(
+            self.Graph,
+            pos=pos,
+            ax=ax,
+            min_source_margin=15,
+            min_target_margin=15,
+        )
+
+        nx.draw_networkx(self.Graph, pos, **options)
+
+        tr_figure = ax.transData.transform
+        tr_axes = fig.transFigure.inverted().transform
+
+        icon_size = (ax.get_xlim()[1] - ax.get_xlim()[0]) * 0.02
+        icon_center = icon_size / 2.0
+
+        for n in self.Graph.nodes:
+            xf, yf = tr_figure(pos[n])
+            xa, ya = tr_axes((xf, yf))
+            a = plt.axes([xa + icon_center / 1.2, ya - icon_center, icon_size, icon_size])
+            a.imshow(self.Graph.nodes[n]['image'])
+            a.axis("off")
+        plt.show()
